78c69e738d5334db17cc9eee6e189409ce3bc822
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2014-07-23 10:24:09 +0200

    Comments

--- requirements.go
+++ requirements.go
@@ -2,10 +2,12 @@ package main
 
 type Requirements map[string]string
 
+// Creates a new, empty requirement map
 func noRequirements() Requirements {
 	return make(map[string]string)
 }
 
+// Merge a set of requirements into an existing set of requirements
 func addRequirements(oldReqs Requirements, newReqs Requirements) {
 	for k, v := range newReqs {
 		oldReqs[k] = v
